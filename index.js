530f4b9891772faa868f80f04722646b43469462
Bane Debeljevic <bane.dd@gmail.com>
2019-03-19 09:16:10 -0400

    Minor refactor to reduce code duplication

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ const logger = createLogger({
   transports: [new transports.Console()]
 });
 
+const stats = (url, type, files) =>
+  new Object({
+    url: url,
+    type: type,
+    numberRequested: files.length,
+    numberNotFound: files.filter(file => file.status === 404).length,
+    totalSize: files.reduce((acc, cur) => acc + cur.size, 0) // Size in bytes
+  });
+
+const processForSize = (files, dataReceived) => {
+  for (let i = 0; i < files.length; i++) {
+    let received = dataReceived.find(file => file.requestId === files[i].requestId);
+    if (received) files[i].size = received.encodedDataLength > 0 ? received.encodedDataLength : received.dataLength;
+  }
+};
+
 (async args => {
   if (!args || args.length < 3 || typeof args[2] !== "string") {
     logger.error("Please provide a URL to use");
@@ -14,7 +30,8 @@ const logger = createLogger({
 
   const url = args[2];
   const images = [];
-  const imageDataReceived = [];
+  const dataReceived = [];
+  const bundle = [];
 
   const browser = await puppeteer.launch({
     args: ["--no-sandbox", "--disable-setuid-sandbox"]
@@ -22,37 +39,31 @@ const logger = createLogger({
 
   const page = await browser.newPage();
 
-  const getPageAndProcessDataReceived = async (page, url, images, imageDataReceived) => {
+  const getPageAndProcessDataReceived = async (page, url, dataReceived, ...collections) => {
     await page.goto(url, { timeout: 0, waitUntil: "networkidle0" });
-
-    for (let i = 0; i < images.length; i++) {
-      let received = imageDataReceived.find(item => item.requestId === images[i].requestId);
-      if (received) images[i].size = received.encodedDataLength > 0 ? received.encodedDataLength : received.dataLength;
-    }
+    collections.forEach(collection => processForSize(collection, dataReceived));
   };
 
   page.on("request", request => request.continue());
 
-  page._client.on("Network.dataReceived", event => imageDataReceived.push(event));
+  page._client.on("Network.dataReceived", event => dataReceived.push(event));
 
   page._client.on("Network.responseReceived", event => {
+    const bundleTypes = ["Document", "Font", "Script", "Stylesheet"];
+    const eventData = { url: event.response.url, status: event.response.status, requestId: event.requestId, size: 0 };
     if (event.type === "Image") {
-      images.push({ url: event.response.url, status: event.response.status, requestId: event.requestId, size: 0 });
+      images.push(eventData);
+    } else if (bundleTypes.includes(event.type)) {
+      bundle.push(eventData);
     }
   });
 
   await page.setRequestInterception(true);
   await page.setCacheEnabled(false);
-  await getPageAndProcessDataReceived(page, url, images, imageDataReceived);
+  await getPageAndProcessDataReceived(page, url, dataReceived, images, bundle);
 
-  const imageStats = {
-    url: url,
-    numberRequested: images.length,
-    numberNotFound: images.filter(img => img.status === 404).length,
-    totalSize: images.reduce((acc, cur) => acc + cur.size, 0)
-  };
-
-  logger.info(JSON.stringify(imageStats, null, 2));
+  logger.info(JSON.stringify(stats(url, "images", images), null, 2));
+  logger.info(JSON.stringify(stats(url, "bundle", bundle), null, 2));
 
   await browser.close();
 
